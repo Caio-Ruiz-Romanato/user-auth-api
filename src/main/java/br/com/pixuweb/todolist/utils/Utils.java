953bfd324ff55e15dc1a6061da845ead7d66bca1
Caio Romanato Ruiz <caio.ruiz@dpsp.com.br>
2025-02-07 09:30:12 -0300

    user-auth-api

---
+++ src/main/java/br/com/pixuweb/todolist/utils/Utils.java
@@ -0,0 +1,32 @@
+package br.com.pixuweb.todolist.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Utils {
+
+    public static void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    private static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        return emptyNames.toArray(new String[0]);
+    }
+}
